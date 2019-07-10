21b2337109c653ca96b2782e2a5e75a28cde184d
emmanuel aubrey <whoisemmanuel@gmail.com>
2019-07-10 16:40:29 -0400

    Added mouse events for player and HP

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,35 +3,32 @@ ctx.font = '30px Arial';
  
 var HEIGHT = 500;
 var WIDTH = 500;
-var message = 'Bouncing';
+var timeWhenGameStarted = Date.now();   //return time in ms
  
 var player = {
         x:50,
         spdX:30,
         y:40,
         spdY:5,
-        name:'P',      
+        name:'P',
+        hp:10, 
 };
  
 var enemyList = {};
  
-Enemy('E1',150,350,10,15);
-Enemy('E2',250,350,10,-15);
-Enemy('E3',250,150,10,-8);
  
- 
-function getDistanceBetweenEntity(entity1,entity2){     //return distance (number)
+getDistanceBetweenEntity = function (entity1,entity2){  //return distance (number)
         var vx = entity1.x - entity2.x;
         var vy = entity1.y - entity2.y;
         return Math.sqrt(vx*vx+vy*vy);
 }
  
-function testCollisionEntity(entity1,entity2){  //return if colliding (true/false)
+testCollisionEntity = function (entity1,entity2){       //return if colliding (true/false)
         var distance = getDistanceBetweenEntity(entity1,entity2);
         return distance < 30;
 }
  
-function Enemy(id,x,y,spdX,spdY){
+Enemy = function (id,x,y,spdX,spdY){
         var genericEnemy = {
                 x:x,
                 spdX:spdX,
@@ -41,28 +38,41 @@ function Enemy(id,x,y,spdX,spdY){
                 id:id,
         };
         enemyList[id] = genericEnemy;
+       
+}
+ 
+document.onmousemove = function(mouse){
+        var mouseX = mouse.clientX;
+        var mouseY = mouse.clientY;
+       
+        player.x = mouseX;
+        player.y = mouseY;
+}
+ 
+updateEntity = function (gameEntity){
+        updateEntityPosition(gameEntity);
+        drawEntity(gameEntity);
+}
+updateEntityPosition = function(gameEntity){
+        gameEntity.x += gameEntity.spdX;
+        gameEntity.y += gameEntity.spdY;
+                       
+        if(gameEntity.x < 0 || gameEntity.x > WIDTH){
+                gameEntity.spdX = -gameEntity.spdX;
+        }
+        if(gameEntity.y < 0 || gameEntity.y > HEIGHT){
+                gameEntity.spdY = -gameEntity.spdY;
+        }
+}
+ 
+ 
+drawEntity = function(gameEntity){
+        ctx.fillText(gameEntity.name,gameEntity.x,gameEntity.y);
 }
  
  
  
-setInterval(update,40);
- 
-function updateEntity(something){
-        something.x += something.spdX;
-        something.y += something.spdY;
-        ctx.fillText(something.name,something.x,something.y);
-               
-               
-        if(something.x < 0 || something.x > WIDTH){
-                something.spdX = -something.spdX;
-        }
-        if(something.y < 0 || something.y > HEIGHT){
-                something.spdY = -something.spdY;
-        }
-}
- 
- 
-function update(){
+update = function(){
         ctx.clearRect(0,0,WIDTH,HEIGHT);
        
         for(var key in enemyList){
@@ -70,13 +80,26 @@ function update(){
                
                 var isColliding = testCollisionEntity(player,enemyList[key]);
                 if(isColliding){
-                        console.log('Colliding!');
+                        player.hp = player.hp - 1;
+                        if(player.hp <= 0){
+                                var timeSurvived = Date.now() - timeWhenGameStarted;
+                               
+                                console.log("You lost! You survived for " + timeSurvived + " ms.");
+                                timeWhenGameStarted = Date.now();
+                                player.hp = 10;
+                        }
                 }
                
         }
        
-        updateEntity(player);
-       
-       
-       
+        drawEntity(player);
+        ctx.fillText(player.hp + " Hp",0,30);
 }
+ 
+Enemy('E1',150,350,10,15);
+Enemy('E2',250,350,10,-15);
+Enemy('E3',250,150,10,-8);
+ 
+ 
+setInterval(update,40);
+ 
